3870d4c2ada01390fed50a8cfbf3c74f0e1dcefe
Elonatech Nigeria limited <102363945+Elonatech@users.noreply.github.com>
2023-12-01 09:53:45 +0100

    Added product controller

--- controller/productController.js
+++ controller/productController.js
@@ -1,8 +1,22 @@
 const Product = require('../models/productModel');
 const cloudinary = require('../lib/cloudinary');
 
-const createProduct =  (req , res) =>{
-
+const createProduct =  async (req , res) =>{
+    let result;
+    if (req.file) {
+      result =  await cloudinary.uploader.upload(req.file.path);
+    } else {
+        return res.send('cloudinary path is undefined')
+    }
+    
+    const newProduct = await Product.create({
+                   title: req.body.title,
+                   description: req.body.description,
+                   author: req.body.author,
+                   category: req.body.category,
+                   cloudinary_id: result.secure_url,
+    });
+    return res.status(201).json({ newProduct });
 }
 
 const  getAllProducts = (req , res) =>{
